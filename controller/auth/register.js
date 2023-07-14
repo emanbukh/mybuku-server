528d02bf5037fc352e8f17aa5c859158dc244e53
emanbukh <emanbukhori@gmail.com>
2023-07-14 15:42:15 +0800

    correct register.jsx

--- controller/auth/register.js
+++ controller/auth/register.js
@@ -1,23 +1,43 @@
 import query from "../../db/index.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+
+const TOKEN_SECRET = "25VNclweOQgzAk0bmzBdTQoYnqshZwll";
+
+const generateAccessToken = (userData) => {
+  return jwt.sign(userData, TOKEN_SECRET, { expiresIn: "1800s" });
+};
 
 const register = async (req, res) => {
   try {
     const body = req.body;
-    // hash password
+    // Hash password
     const salt = bcrypt.genSaltSync(10);
     const hashedPassword = bcrypt.hashSync(body.password, salt);
     const dbRes = await query(
       "INSERT INTO users (email, username, password, is_admin) VALUES ($1, $2, $3, $4)",
       [body.email, body.username, hashedPassword, body.isAdmin]
     );
-    console.log(dbRes);
+
+    // Retrieve the inserted user from the database
+    const user = await query("SELECT * FROM users WHERE email = $1", [body.email]);
+
+    // Generate JWT token
+    const token = generateAccessToken({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      isAdmin: user.is_admin,
+    });
+
     const serverRes = {
       message: "A user created",
+      data: user,
+      jwt: token,
     };
     res.status(200).json(serverRes);
   } catch (error) {
-    console.log(error); // Log the error object
+    console.error(error); // Log the error object
     const { name, table, constraint, detail } = error;
     const serverRes = {
       message: detail,
